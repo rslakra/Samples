b91f23609d795729f06236fc5b014c78e2a712f9
Rohtash Lakra <rslakra@users.noreply.github.com>
2025-12-14 13:42:54 -0800

    update appsuite-spring v0.0.32 and appsuite-core 0.0.102

--- SpringBoot/springboot-service-jsp/src/main/java/com/rslakra/springbootjsp/dto/Book.java
+++ SpringBoot/springboot-service-jsp/src/main/java/com/rslakra/springbootjsp/dto/Book.java
@@ -1,6 +1,10 @@
 package com.rslakra.springbootjsp.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
